5f7751d95518de67fd4e3a4b1386e6ab14951c3f
NelsonCad <NelsonCad97@gmail.com>
2019-09-10 23:57:15 -0700

    started on the code for saving articles

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,16 @@
 $(function () {
 
-    $(".save-article")
+    $(document).on("click", ".save-article", function () {
+        let id = $(this).attr("value")
+
+        $.ajax({
+            method: "POST",
+            url: "/savedarticles/" + id
+        }).then(function (data) {
+            console.log(data)
+        }).catch(function (err) {
+            console.log(err);
+        })
+    })
 
 })
